dffcf42bd7345b9e20711e58a1a7adbd221f5258
Jim Gay <jim@saturnflyer.com>
2013-08-19 20:27:23 -0400

    add a note about why surroundings is defined and set it to private

--- lib/surrounded/railtie.rb
+++ lib/surrounded/railtie.rb
@@ -6,6 +6,17 @@ module Surrounded
     initializer 'surrounded.active_record' do |app|
       ActiveRecord::Base.send(:include, ::Surrounded)
       ActiveRecord::Base.class_eval {
+        # Ideally this should hook into the initialization process
+        # and set the @__surroundings__ collection there.
+        # Currently I haven't found a way to do that.
+        #
+        # Surrounded uses respond_to_missing? and method_missing
+        # which are two methods used when an ActiveRecord object
+        # is initialized.
+        #
+        # The after_initialize callback happens after those methods
+        # are used.
+        #
         define_method(:surroundings){
           @__surroundings__ ||= []
         }
