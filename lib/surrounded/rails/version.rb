a92ebc9220d9dfac8b7cb5e9043eae01f845d237
Jim Gay <jim@saturnflyer.com>
2014-01-16 10:54:52 -0500

    alter the approach because a private method was removed from rails and there is still no good way to hook in at the right time

--- lib/surrounded/rails/version.rb
+++ lib/surrounded/rails/version.rb
@@ -1,5 +1,5 @@
 module Surrounded
   module Rails
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
   end
 end
--- lib/surrounded/railtie.rb
+++ lib/surrounded/railtie.rb
@@ -6,18 +6,15 @@ module Surrounded
     initializer 'surrounded.active_record' do |app|
       ActiveRecord::Base.class_eval {
         include Surrounded
-        # This relies on a private method (present in Rails 3 and 4)
-        # because there is no earlier hook to set this variable.
+        # There is no earlier hook to set this variable.
         # The after_initialize callback occurs to late because
         # the initialize method has a procedure which calls
         # respond_to? and Surrounded adds respond_to_missing?
         # to get information from the context (which is the first
         # item in the @__surroundings__ collection).
-        def init_internals_with_surrounded
-          Surrounded.create_surroundings(self)
-          init_internals_without_surrounded
+        def surroundings
+          @__surroundings__ ||= []
         end
-        alias_method_chain :init_internals, :surrounded
       }
     end
 
