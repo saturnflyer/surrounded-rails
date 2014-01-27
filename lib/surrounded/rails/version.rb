27e01b9c401d2e53faf72ccb69397f2c53f423bb
Jim Gay <jim@saturnflyer.com>
2014-01-26 21:22:03 -0500

    bump version of surrounded to the latest and change this version

--- lib/surrounded/rails/version.rb
+++ lib/surrounded/rails/version.rb
@@ -1,5 +1,5 @@
 module Surrounded
   module Rails
-    VERSION = "0.1.2"
+    VERSION = "0.2.0"
   end
 end
--- lib/surrounded/railtie.rb
+++ lib/surrounded/railtie.rb
@@ -4,18 +4,7 @@ require "rails"
 module Surrounded
   class Railtie < ::Rails::Railtie
     initializer 'surrounded.active_record' do |app|
-      ActiveRecord::Base.class_eval {
-        include Surrounded
-        # There is no earlier hook to set this variable.
-        # The after_initialize callback occurs to late because
-        # the initialize method has a procedure which calls
-        # respond_to? and Surrounded adds respond_to_missing?
-        # to get information from the context (which is the first
-        # item in the @__surroundings__ collection).
-        def surroundings
-          @__surroundings__ ||= []
-        end
-      }
+      ActiveRecord::Base.send(:include, Surrounded)
     end
 
     initializer 'surrounded.action_controller' do |app|
